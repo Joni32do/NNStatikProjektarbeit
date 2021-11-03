5c1a3a2e9690e6ecb2213dd67d62fdf6b6397f87
Jonathan Schnitzler <jonathanschnitzler@web.de>
2021-11-03 18:06:30 +0100

    Unnecessary

--- NNStatikProjektarbeit/Projektarbeit/Ansys/Environment/cleanup-ansys-vdt-ibb2-03-8572.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set LOCALHOST=%COMPUTERNAME%
-if /i "%LOCALHOST%"=="vdt-ibb2-03" (taskkill /f /pid 1268)
-if /i "%LOCALHOST%"=="vdt-ibb2-03" (taskkill /f /pid 8572)
-
-del /F cleanup-ansys-vdt-ibb2-03-8572.bat
