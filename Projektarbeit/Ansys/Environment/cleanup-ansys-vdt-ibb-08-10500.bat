adc54827e87d4760b717ab5e3c5e42b974bf4c95
Jonathan Schnitzler <jonathanschnitzler@web.de>
2021-12-03 17:01:29 +0100

    Final Commit 3.12
    - NSEL, ESEL angeschaut
    - Contour Plot Filmchen
    - Erstes Neuronales Netzwerk und Minimierungsfunktionen

--- Projektarbeit/Ansys/Environment/cleanup-ansys-vdt-ibb-08-10500.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set LOCALHOST=%COMPUTERNAME%
-if /i "%LOCALHOST%"=="vdt-ibb-08" (taskkill /f /pid 3412)
-if /i "%LOCALHOST%"=="vdt-ibb-08" (taskkill /f /pid 10500)
-
-del /F cleanup-ansys-vdt-ibb-08-10500.bat
