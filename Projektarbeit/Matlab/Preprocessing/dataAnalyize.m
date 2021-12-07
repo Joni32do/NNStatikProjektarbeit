a55f077670c3857f9992844c99d5f7c8bd4ca285
Jonathan Schnitzler <jonathanschnitzler@web.de>
2021-12-07 17:19:16 +0100

    Arbeiten an Random Generation

--- Projektarbeit/Matlab/Preprocessing/dataAnalyize.m
+++ Projektarbeit/Matlab/Preprocessing/dataAnalyize.m
@@ -1,23 +1,26 @@
-load("1911SimulationMesh_0-05.mat");
-Sim005 = X;
-Sim005Y = Y;
-
-load("1911SimulationMesh_0-1.mat");
-Sim01 = X;
-Sim01Y = Y;
-
-load("1911SimulationMesh_0-1Force250000+00.mat");
-Sim01F = X;
-Sim01FY = Y;
-
-
-format shorte
-k = 15;
-
-
-
-%Data befor 15 is rubbish -> why
-for k = 14:28
-    comp = [Sim005Y(k,:); Sim01Y(k,:); Sim01FY(k,:)]
-    plotData(Sim01(k,:),"First 15");
+% load("1911SimulationMesh_0-05.mat");
+% Sim005 = X;
+% Sim005Y = Y;
+% 
+% load("1911SimulationMesh_0-1.mat");
+% Sim01 = X;
+% Sim01Y = Y;
+% 
+% load("1911SimulationMesh_0-1Force250000+00.mat");
+% Sim01F = X;
+% Sim01FY = Y;
+% 
+% 
+% format shorte
+% k = 15;
+% 
+% 
+% 
+% %Data befor 15 is rubbish -> why
+% for k = 14:28
+%     comp = [Sim005Y(k,:); Sim01Y(k,:); Sim01FY(k,:)]
+%     plotData(Sim01(k,:),"First 15");
+% end
+for k = 1:100
+    plotData([X(k,:) ones(1,5)],"Hallo");
 end
